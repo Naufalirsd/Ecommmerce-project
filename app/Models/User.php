7f7287901c8d8dc437cfe4969622c4c365f6ed27
Naufal Irsyad Ihsan <naufalirsd.ppqita@gmail.com>
2025-04-08 09:42:06 +0700

    update order and user resource

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,11 +39,12 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function orders() {
+        return $this->hasMany(Order::class);
     }
 }
